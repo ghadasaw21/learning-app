b3f433c8ecbdc1aa399783907dace57a1dcc489c
ghadasaw21 <academyghadaphone@icloud.com>
2024-10-23 14:21:06 +0300

    Initial Commit

---
+++ newme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newme
+//
+//  Created by Ghada al ajmi on 20/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newme/newmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  newmeApp.swift
+//  newme
+//
+//  Created by Ghada al ajmi on 20/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct newmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
